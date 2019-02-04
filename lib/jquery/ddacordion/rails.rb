10a7b52e1d1bab8a5f1a87790e3166753eff8508
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 09:53:52 +0900

    initial commit

---
+++ lib/jquery/ddacordion/rails.rb
@@ -0,0 +1,10 @@
+require "jquery/ddacordion/rails/version"
+
+module Jquery
+  module Ddacordion
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery/ddacordion/rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Ddacordion
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
